a4babc7c61a2512dc5e82f105b70495b3313a019
Liam Rowley <liam.rowley@hotmail.co.uk>
2020-02-02 23:11:59 +0000

    prevent same movie appearing twice in a row, add loading state to movie hook

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { useMovie } from '../hooks/useMovie';
 
 export const App = () => {
-  const movie = useMovie();
+  const [movie, movieActions, movieState] = useMovie();
+  console.log(movieState);
+  const { fetchMovie } = movieActions;
   return (
     <div>
-      <button onClick={() => movie.fetchMovie(7)}></button>
+      <button onClick={() => fetchMovie(8)}>{ movieState.isLoading ? 'Loading' : 'Fetch Movie' }</button>
       App
+      <div>
+        <h2>{ movie.title }</h2>
+        <p>{ movie.overview }</p>
+      </div>
     </div>
   );
 }
--- src/hooks/useMovie/useProvideMovie.js
+++ src/hooks/useMovie/useProvideMovie.js
@@ -5,11 +5,10 @@ import { HORROR_ID } from '../../constants';
 import { randomNumBetween } from '../../helpers';
 
 export const useProvideMovie = () => {
-
-  const [movie, setMovie] = useState();
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovie = async (voteAverage = 9) => {
-
     // Params that determine the range of horror movies returned
     const params = {
       'with_genres': HORROR_ID,
@@ -19,37 +18,52 @@ export const useProvideMovie = () => {
 
     console.log('Initial params: ', params);
 
-    // Initial list of movies within paramter range
-    const movieList = await moviedb.get('/discover/movie', { params });
-    console.log('Initial data: ', movieList);
+    try {
+      setIsLoading(true);
+      // Initial list of movies within paramter range
+      const movieList = await moviedb.get('/discover/movie', { params });
+      console.log('Initial data: ', movieList);
 
-    // Select a random page from the results and get the movies on that page
-    const totalPages = movieList.data.total_pages;
-    const selectedPageIndex = randomNumBetween(1, totalPages);
+      // Select a random page from the results and get the movies on that page
+      const totalPages = movieList.data.total_pages;
+      const selectedPageIndex = randomNumBetween(1, totalPages);
 
-    console.log('Total pages: ', totalPages);
-    console.log('Selected page: ', selectedPageIndex);
+      console.log('Total pages: ', totalPages);
+      console.log('Selected page: ', selectedPageIndex);
 
-    const paramsWithPage = {
-      ...params,
-      'page': selectedPageIndex
+      const paramsWithPage = {
+        ...params,
+        'page': selectedPageIndex
+      }
+
+      const selectedPage = await moviedb.get('/discover/movie', { params: paramsWithPage });
+      console.log(selectedPage);
+
+      // Select a random movie from the selected page and get its full information
+      const selectedMovieIndex = randomNumBetween(0, selectedPage.data.results.length - 1);
+      const selectedMovieId = selectedPage.data.results[selectedMovieIndex].id;
+      console.log(selectedPage.data.results[selectedMovieIndex]);
+
+      if (selectedMovieId === movie.id) {
+        console.log('Same as previous movie');
+        fetchMovie(voteAverage);
+      }
+
+      const selectedMovie = await moviedb.get(`/movie/${selectedMovieId}`);
+      setMovie(selectedMovie.data);
+    } catch (error) {
+      console.error(error);
     }
-
-    const selectedPage = await moviedb.get('/discover/movie', { params: paramsWithPage });
-    console.log(selectedPage);
-
-    // Select a random movie from the selected page and get its full information
-    const selectedMovieIndex = randomNumBetween(0, selectedPage.data.results.length - 1);
-    const selectedMovieId = selectedPage.data.results[selectedMovieIndex].id;
-    console.log(selectedPage.data.results[selectedMovieIndex]);
-
-    const selectedMovie = await moviedb.get(`/movie/${selectedMovieId}`);
-
-    console.log(selectedMovie.data);
+    setIsLoading(false);
   }
 
-  return {
+  const actions = {
     fetchMovie
   };
 
+  const state = {
+    isLoading
+  };
+
+  return [movie, actions, state];
 }
