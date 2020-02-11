50462990fedc27e5a4afdb916f4b3f3bba6357f1
Liam Rowley <liam.rowley@hotmail.co.uk>
2020-02-11 11:05:07 +0000

    style header component, rename spacing vars to not refrence 'padding' in name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from './style.module.scss';
+
 export const Header = () => {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/pinned">Pinned Movies</Link>
+    <header className={styles['header']}>
+      <nav className={styles['nav']}>
+        <Link to="/" className={styles['nav-item']} activeClassName={styles['nav-item--active']}>Home</Link>
+        <Link to="/pinned" className={styles['nav-item']} activeClassName={styles['nav-item--active']}>Pinned Movies</Link>
+      </nav>
     </header>
   );
 }
